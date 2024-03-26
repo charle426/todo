e9662281f8583843daa75e47a72054845508f45d
charle426 <114236505+charle426@users.noreply.github.com>
2024-03-26 12:27:43 +0100

    fancies added

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -49,13 +49,19 @@ function App() {
         }
       >
         <div className="flex gap-4 items-center w-full">
-          <div>
-            <input
-              type="checkbox"
-              className="checkedTodo"
-              checked={item.complete}
-              onChange={() => checked(index)}
-            />
+          <div className="">
+            <label className="container">
+              <input
+                checked={item.complete}
+                type="checkbox"
+                id=""
+                className="checkedTodo "
+                onChange={() => checked(index)}
+              />
+              <div className="checkmark"></div>
+            </label>
+
+            {/* <label htmlFor="cbx" className="cbx"></label> */}
           </div>
           <div>
             <p>{item.text}</p>
@@ -65,8 +71,45 @@ function App() {
           className="relative flex justify-end cursor-pointer items-center"
           onClick={() => setTodoArr(todoArr.filter((task, id) => index !== id))}
         >
-          <div className="h-[25px] w-[2px] bg-slate-800 -translate-y-[4px] translate-x-[2px] -rotate-[40deg]"></div>
-          <div className="h-[25px] w-[2px] -translate-y-[2px] bg-slate-800 rotate-[40deg]"></div>
+          <button className="button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 69 14"
+              className="svgIcon bin-top"
+            >
+              <g clipPath="url(#clip0_35_24)">
+                <path
+                  fill="black"
+                  d="M20.8232 2.62734L19.9948 4.21304C19.8224 4.54309 19.4808 4.75 19.1085 4.75H4.92857C2.20246 4.75 0 6.87266 0 9.5C0 12.1273 2.20246 14.25 4.92857 14.25H64.0714C66.7975 14.25 69 12.1273 69 9.5C69 6.87266 66.7975 4.75 64.0714 4.75H49.8915C49.5192 4.75 49.1776 4.54309 49.0052 4.21305L48.1768 2.62734C47.3451 1.00938 45.6355 0 43.7719 0H25.2281C23.3645 0 21.6549 1.00938 20.8232 2.62734ZM64.0023 20.0648C64.0397 19.4882 63.5822 19 63.0044 19H5.99556C5.4178 19 4.96025 19.4882 4.99766 20.0648L8.19375 69.3203C8.44018 73.0758 11.6746 76 15.5712 76H53.4288C57.3254 76 60.5598 73.0758 60.8062 69.3203L64.0023 20.0648Z"
+                ></path>
+              </g>
+              <defs>
+                <clipPath id="clip0_35_24">
+                  <rect fill="white" height="14" width="69"></rect>
+                </clipPath>
+              </defs>
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 69 57"
+              className="svgIcon bin-bottom"
+            >
+              <g clipPath="url(#clip0_35_22)">
+                <path
+                  fill="black"
+                  d="M20.8232 -16.3727L19.9948 -14.787C19.8224 -14.4569 19.4808 -14.25 19.1085 -14.25H4.92857C2.20246 -14.25 0 -12.1273 0 -9.5C0 -6.8727 2.20246 -4.75 4.92857 -4.75H64.0714C66.7975 -4.75 69 -6.8727 69 -9.5C69 -12.1273 66.7975 -14.25 64.0714 -14.25H49.8915C49.5192 -14.25 49.1776 -14.4569 49.0052 -14.787L48.1768 -16.3727C47.3451 -17.9906 45.6355 -19 43.7719 -19H25.2281C23.3645 -19 21.6549 -17.9906 20.8232 -16.3727ZM64.0023 1.0648C64.0397 0.4882 63.5822 0 63.0044 0H5.99556C5.4178 0 4.96025 0.4882 4.99766 1.0648L8.19375 50.3203C8.44018 54.0758 11.6746 57 15.5712 57H53.4288C57.3254 57 60.5598 54.0758 60.8062 50.3203L64.0023 1.0648Z"
+                ></path>
+              </g>
+              <defs>
+                <clipPath id="clip0_35_22">
+                  <rect fill="white" height="57" width="69"></rect>
+                </clipPath>
+              </defs>
+            </svg>
+          </button>
         </div>
       </div>
     );
@@ -120,7 +163,12 @@ function App() {
 
   return (
     <section className="flex gap-5 flex-col justify-center items-center min-h-[100vh]">
-      <div className="flex items-center justify-between p-2 ring ring-blue-900">
+      <div>
+        <h1 className="text-[4rem]">
+          TO DO
+        </h1>
+      </div>
+      <div className="flex items-center justify-between p-2 ring ring-gray-400">
         <div>
           <input
             type="text"
@@ -131,15 +179,33 @@ function App() {
           />
         </div>
         <div
-          className="hover:ring-1 hover:ring-blue-900 p-3 bg-blue-700"
+          className=""
           onClick={addTodo}
         >
-          Add Todo
+          <button
+            title="Add New Todo"
+            className="group cursor-pointer outline-none hover:rotate-90 duration-300"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50px"
+              height="50px"
+              viewBox="0 0 24 24"
+              className="stroke-zinc-400 fill-none  group-active:stroke-zinc-200 group-active:fill-zinc-100 group-active:duration-0 duration-300"
+            >
+              <path
+                d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+                strokeWidth="1.5"
+              ></path>
+              <path d="M8 12H16" strokeWidth="1.5"></path>
+              <path d="M12 16V8" strokeWidth="1.5"></path>
+            </svg>
+          </button>
         </div>
       </div>
       <div className="divide-y flex flex-col gap-3 items-center w-[600px] justify-center">
         <div className="flex flex-col items-center divide-y justify-center *:my-2 *:w-full w-full">
-          {checkedOut.length  ? mappedArr : <p>No tasks here</p>}
+          {checkedOut.length ? mappedArr : <p>No tasks here</p>}
         </div>
       </div>
       <div className="flex justify-between space-x-11 items-center p-5">
